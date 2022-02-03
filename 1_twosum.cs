0fa28ab27c5430bf6ab176afbf720db27f474c12
troysantiago-mac <troysantiago2000@gmail.com>
2022-02-03 10:28:40 +0800

    fixed comments on 1_twosum.cs

--- 1_twosum.cs
+++ 1_twosum.cs
@@ -1,13 +1,12 @@
 public class Solution
 {
+    // more about the problem here
+    // https://leetcode.com/problems/two-sum/
+    // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    // You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    // You can return the answer in any order.
     public int[] TwoSum(int[] nums, int target)
     {
-        // more about the problem here
-        // https://leetcode.com/problems/two-sum/
-        // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-        // You may assume that each input would have exactly one solution, and you may not use the same element twice.
-        // You can return the answer in any order.
-
         //declarations
         Dictionary<int, int> _tracker = new Dictionary<int, int>();
         int[] _solution = new int[2];
@@ -22,9 +21,10 @@ public class Solution
         //logic
         for (int i = 0; i < _arrayLength; i++)
         {
-
+            //lets get the remainder
             int _remainder = target - nums[i];
 
+            //check if remainder is in the tracker 
             if (_tracker.ContainsKey(_remainder))
             {
 
@@ -36,6 +36,8 @@ public class Solution
                 return _solution = new int[] { _trackerIndex, i };
             }
 
+            //lets update our tracker we use values to replace, and not add, 
+            //because an error will be thrown if the same key already exist
             _tracker[nums[i]] = i;
         }
 
