2f020fcbe61863931a7d72c76a905d75428cfeb5
troysantiago-mac <troysantiago2000@gmail.com>
2022-02-19 09:00:22 +0800

    added solution for 70

---
+++ 70_climbing_stairs.cs
@@ -0,0 +1,29 @@
+public class Solution
+{
+    // more info about the problem here
+    // https://leetcode.com/problems/climbing-stairs/
+    // You are climbing a staircase. It takes n steps to reach the top.
+    // Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+    public int ClimbStairs(int p_n)
+    {
+        //validations
+        if (p_n == 0 || p_n == 1)
+        {
+            return 1;
+        }
+
+        //declarations
+        int _pre = 1;
+        int _curr = _pre;
+
+        //logic
+        for (int __i = 2; __i <= p_n; __i++)
+        {
+            int __t = _curr + _pre;
+            _pre = _curr;
+            _curr = __t;
+        }
+
+        return _curr;
+    }
+}
